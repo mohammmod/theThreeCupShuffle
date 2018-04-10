441799bc638c2897efbf73d1f063566ee2dbc70e
mohammmod <mohammad.sawas2@outlook.com>
2018-04-10 22:14:27 +0200

    initilaize my project

---
+++ src/main/java/at/refugeescode/threecupshuffel/ThreecupshuffelApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.threecupshuffel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreecupshuffelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreecupshuffelApplication.class, args);
+	}
+}
